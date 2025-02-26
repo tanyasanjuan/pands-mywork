449b95cc3bb77b298ffb640f48c40195499c324e
Tanya <tanya.sanjuan@gmail.com>
2025-02-26 17:17:40 +0000

    while loops - numbers from 2 to 100

---
+++ week04-flow/lab4.2.1-evens.py
@@ -0,0 +1,10 @@
+# This program prints out all the even numbers from 2 to 100
+# Author: Tanya San Juan
+
+numberTo = 100
+evenNum = 2
+
+while evenNum <= numberTo:
+    print (evenNum)
+    evenNum += 2
+
