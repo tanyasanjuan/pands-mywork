be5a17d24a539130c7d20ecc36bcafc3ea54430e
Tanya <tanya.sanjuan@gmail.com>
2025-02-26 15:43:00 +0000

    if and else - the number is even or odd

---
+++ week04-flow/lab4.1.1-isEven.py
@@ -0,0 +1,9 @@
+# This program asks the user to enter a number 
+# And says to the user if the number is even or odd
+# author: Tanya San Juan
+
+number = int(input ('enter an integer: '))
+if (number % 2 ) == 0:
+    print (f'{number} is an even number')
+else : 
+    print (f'{number} is an odd number')
