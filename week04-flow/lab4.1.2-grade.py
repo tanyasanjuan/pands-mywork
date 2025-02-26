a74038491e5f371ab8323d5da02d912a67c177f1
Tanya <tanya.sanjuan@gmail.com>
2025-02-26 23:13:54 +0000

    Students grades_rounded percentages

--- week04-flow/lab4.1.2-grade.py
+++ week04-flow/lab4.1.2-grade.py
@@ -15,5 +15,29 @@ elif percentage < 60: # Between 50 and 59
     print ('Meriti')
 elif percentage < 70: # Betweeen 60 and 69
     print ('Merit2')
-else: # The onlu option left is between 70 and 100 
+else: # The only option left is between 70 and 100 
+    print ('Distintion')
+
+# How you will modify the program to round the percentage, 
+# (e.g., 69.5 becomes 70 and qualifies as a Distinction)
+# We can use Python’s round() The percentages are rounded 
+# https://docs.python.org/3/library/functions.html#round
+
+percentage = float (input ('Enter the percentage: '))
+#print (percentage)
+
+if percentage < 0 or percentage > 100: 
+    print ('Please enter a number between 0 and 100')
+
+else: round_percentage = round (percentage)
+
+if round_percentage < 40: # we it is greatet than 0 
+    print ('Fail')
+elif round_percentage < 50: # Between 40 and 49
+    print ('Pass')
+elif round_percentage < 60: # Between 50 and 59 
+    print ('Meriti')
+elif round_percentage < 70: # Betweeen 60 and 69
+    print ('Merit2')
+else: # The only option left is between 70 and 100 
     print ('Distintion')
