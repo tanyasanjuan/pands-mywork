d4b17dbcaf2c88d26b58d7c5188d9efbf21745c7
Tanya <tanya.sanjuan@gmail.com>
2025-03-12 03:31:33 +0000

    quiz

---
+++ week05-datastructures/quiz.py
@@ -0,0 +1,41 @@
+numberOfQuestions = 5
+averageAge = 23.4
+debugMode = True
+name = "joe"
+ages = []
+months = ('Jan', 'Feb', 'Mar')
+book = {}
+stuff = [ 12 , 'Fred', False, {}]
+someone = dict(firstname = "joe")
+me = {
+"firstName" : "Andrew",
+"teaching" : [{
+"courseName" : "programming",
+"semester" : 1
+},{
+"courseName" : "Data Representation",
+"semester" : 2
+}
+]
+}
+
+# What are the variable types of the following variables in the code above
+'''
+a. numberOfQuestions: int
+b. averageAge: float
+c. debugMode: bool
+d. name: str
+e. ages: list
+f. months: tuple
+g. months[1]: str
+h. book: dict
+i. stuff: list
+j. stuff[2]: bool
+k. someone: dict
+l. someone["firstname"]: str
+m. me: dict
+n. me["teaching"]: list
+o. me["teaching"][0]["semester"]: int
+#p is a trick question look at it carefully
+p. me["teaching"][0]["coursename"]: str
+'''
