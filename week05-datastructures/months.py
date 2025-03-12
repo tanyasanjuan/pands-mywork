47c718b63bb764a7d8a85f53f1f381de1913753a
Tanya <tanya.sanjuan@gmail.com>
2025-03-12 03:00:27 +0000

    Months in a tuple

---
+++ week05-datastructures/months.py
@@ -0,0 +1,20 @@
+# This program Create a tuple that stores the months of the year, 
+# from that tuple creates another tuple with just the summer months 
+# and Print print out the summer months (May, June, July one at a time
+# Author: Tanya San Juan
+
+months = ('January', 
+          'February', 
+          'March', 
+          'April', 
+          'May', 
+          'June', 
+          'July', 
+          'August', 
+          'September', 
+          'October', 
+          'November', 
+          'December') 
+summer = months [4:7]
+for month in summer:
+    print (month)
