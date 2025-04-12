c1b6c484a3f615311be3bb80d4e186965e48586e
Tanya <tanya.sanjuan@gmail.com>
2025-04-12 14:21:39 +0100

    week06 tutorial refactor - reading numbers

---
+++ week06/T2.1-readinnumbers.py
@@ -0,0 +1,39 @@
+# Read in two numbers and multiple them
+'''
+num1 = int (input("Enter first number: "))
+num2 = int (input("Enter second number: "))
+
+answer = num1 * num2
+print(f"The answer is: {answer}")
+# If the user enters a decimal number, the program will crash
+# If the user enters a string, the program will crash
+# If the user enters a negative number, the program will work
+# If the user enters a zero, the program will work
+# If the user enters a very large number, the program will work
+# If the user enters a very small number, the program will work
+
+# To avoid user errors, we can use try and except
+# If the user in the first input enters a string, the program will ask for a number again
+try:
+    num1 = int (input("Enter a number: "))
+except ValueError:
+    num1 = int (input("No strings a number: "))
+num2 = int (input("Enter second number: "))
+
+answer = num1 * num2
+print(f"The answer is: {answer}")
+'''
+# If the user in the second attempt enters a string, the program will ask for a number again
+# To avoid user errors, we can use a while loop
+
+num1 = False
+while (num1 == False):
+    try:
+        num1 = int (input("Enter a number: "))
+    except ValueError:
+        print ("Please enter a number: ")
+
+num2 = int (input("Enter second number: "))
+
+answer = num1 * num2
+print(f"The answer is: {answer}")
