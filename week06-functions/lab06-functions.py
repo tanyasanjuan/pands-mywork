d00b1df3b35bfa1b84ca8d9fd44e8ebb9e766769
Tanya <tanya.sanjuan@gmail.com>
2025-04-18 15:07:41 +0100

    Lab06 implemented functions doAdd and readModules.
    Covering points 5 and 6 of the lab.

--- week06-functions/lab06-functions.py
+++ week06-functions/lab06-functions.py
@@ -1,24 +1,10 @@
 # The program write a function that prints out a menu of commands we can perform
 # ie. add, view, and quit.
 # The function should return what the user has selected.
-'''
-def displaymenu ():
-    print("What would you like to do?")
-    print("\t(a) Add new student")
-    print("\t(v) View student")
-    print("\t(q) Quit")
-    choice = input("Type one letter (a/v/q):").strip()
-    # The strip() method removes any leading and trailing whitespace from the string.
-
-    return choice
-
-# Test the function
-choice = displaymenu()
-print(f"You selected: {choice}")
-
 # With the function create a program 
 # That keeps displaying the menu until the user picks quit
 # If the user chooses V then call a function called doView.
+
 def displaymenu ():
     print("What would you like to do?")
     print("\t(a) Add new student")
@@ -28,39 +14,48 @@ def displaymenu ():
     # The strip() method removes any leading and trailing whitespace from the string.
 
     return choice
-def doAdd():
-    print("in adding")
-def doView():
-    print("in viewing")
 
-# main program 
-choice = displaymenu()
-while (choice != "q"):
-    # we could do this with lambda functions, but we will not do that yet
-    if choice == "a":
-        doAdd()
-    elif choice == "v":
-        doView()
-    elif choice != "q":
-        print("\n\nplease select either a,v or q")
-    choice = displaymenu()
-'''
-    # Write the function doAdd (students) 
-    # Read in the student's name 
-    # Read in module names and grades
-    # Store the data in a dictionary 
-    # Add the dictionary to a list
 
-students = []
+# The function readModules should read in the module names and grades
+# Read modules until the user enter a module name of blank.
 def readModules():
-        return []
+    modules = []
+    moduleName = input("\tEnter the first Module name (blank to quit):").strip()
+
+    while moduleName != "":
+        module = {}
+        module ["name"] = moduleName
+        module ["grade"] =int(input("\t\tEnter grade:"))
+        modules.append(module)
+        #read the next module name
+        moduleName = input("\tEnter next module name (blank to quit): ").strip()
+    return modules
+
+
+# The function doAdd should read in the student's name and modules
 def doAdd(students):
         currentstudent = {}
         currentstudent["name"] = input("Enter name: ")
         currentstudent["modules"] = readModules()
-        
+
         students.append(currentstudent)
-    # Test the function
-doAdd(students)
-doAdd(students)
-print(students)
+
+
+# Add the funcionn doView(students)
+# The function doView should display the students and their modules
+def doView(students):
+    print (students)
+
+
+# main program 
+students = []
+choice = displaymenu()
+while (choice != "q"):
+    # we could do this with lambda functions, but we will not do that yet
+    if choice == "a":
+        doAdd(students)
+    elif choice == "v":
+        doView(students)
+    elif choice != "q":
+        print("\n\nplease select either a,v or q")
+    choice = displaymenu()
