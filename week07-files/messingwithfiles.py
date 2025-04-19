6b47510046f4a4606fab96ee2971224f08723318
Tanya <tanya.sanjuan@gmail.com>
2025-04-19 15:58:04 +0100

    Lab07 meesingwithfiles function that overwrite the file.

--- week07-files/messingwithfiles.py
+++ week07-files/messingwithfiles.py
@@ -1,8 +1,7 @@
 # This proram counts how many times the program was run
 # and saves it to a file called count.txt
-# The program will read the file and increment the count by 1
-# and then write the new count to the file.
 
+# The funtion will read the file and oyutput the number. 
 FILENAME = "count.txt"
 def readNumber():
     with open(FILENAME) as f:
@@ -12,3 +11,15 @@ def readNumber():
 #test the readNumber function
 num = readNumber()
 print(num)
+
+# The function will write the number to the file.
+def writeNumber(number):
+    with open(FILENAME, "wt") as f:
+        f.write(str(number))
+# test the writeNumber function
+writeNumber(15)
+# The text file will show 3 in it. 
+# If the number 3 is modified to 4, the text file will show 4 in it.
+# But the number printed will be 3. (The previous number)
+
+ 
