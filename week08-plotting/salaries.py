a73180fb8c347e95c2fe47e6e2088ac59fb6af39
Tanya <tanya.sanjuan@gmail.com>
2025-04-22 11:49:51 +0100

    Program modifies to increases all the salaries by 5%, store in another array

--- week08-plotting/salaries.py
+++ week08-plotting/salaries.py
@@ -6,9 +6,20 @@ import numpy as np
 minSalary = 2000
 maxSalary = 8000
 numberOfEntries = 10
+
 # The seed is set to 10, so that the random numbers are the same every time the program is run.
 # This will make the random numbers the same each time.
 np.random.seed(10) 
+
 salaries = np.random.randint(minSalary, maxSalary, numberOfEntries)
 salariesPlus = salaries + 5000 # This will add 5000 to each number to each value.
 print(salariesPlus)
+
+# Multiply the salaries by 1.05, to give a 5% increase.
+salariesMult = salaries * 1.05
+print(salariesMult)
+# This is a float array, here I convert it to an int array. (it does a floor division)
+# astype(int) will convert the float to an int, but it will not round it.
+# It will just take the whole number part of the float.
+newSalaries = salariesMult.astype(int)
+print (newSalaries)
